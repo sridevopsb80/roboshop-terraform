c4b67fb7a37df4863491b7000aadf48aea47d10c
srikvp <93551165+srikvp@users.noreply.github.com>
2024-11-21 22:21:01 +0530

    updated comments

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 
+#$0-run.sh, $1-dev/prod, $2- apply/destroy
 
-if [ $# -ne 2 ]; then #2 arguments are expected for env and action. if it is not provided
-  echo "$0 env(dev|prod) action(apply|destroy)" # $0-run.sh, $1-dev/prod, $2- apply/destroy
+#2 arguments are expected for env and action. if it is not provided
+
+if [ $# -ne 2 ]; then
+  echo "$0 env(dev|prod) action(apply|destroy)"
   exit 1
 fi
+
 git pull
 rm -rf .terraform
 terraform init -backend-config=env-${1}/state.tfvars
